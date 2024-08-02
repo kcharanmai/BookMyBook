ce7ba4f0f4a650280e8e18c3335ebd7a646d0b03
Kasthala Charanmai <140713988+kcharanmai@users.noreply.github.com>
2024-08-03 00:24:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
 })
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://mern-book-store:Mongodb123@cluster0.oy4ep7e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://mern-book-store:Mongod**@cluster0.oy4ep7e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
